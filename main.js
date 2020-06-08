30792b07077886f3426d5115948ea85039995b56
Julianna <j.p.gti9300@gmail.com>
2020-06-08 20:00:04 +0300

    fix bind

--- main.js
+++ main.js
@@ -4,14 +4,15 @@ function apply(fn, ctx, args = []) {
   return fn.call(ctx, ...args);
 }
 
-function bind(fn, ctx) {
-  return (args = []) => fn.call(ctx, ...args);
+function bind(fn, ctx, ...bindArgs) {
+  return (...args) => fn.call(ctx, ...bindArgs, ...args);
 }
 
-function f() {
+function f(...args) {
   console.log(this);
+  console.log(args);
 }
 apply(f, {}, [1, 2, 3]);
 
-const bindedF = bind(f, {});
-bindedF();
+const bindedF = bind(f, {}, 1, 2);
+bindedF(3, 4, 7);
